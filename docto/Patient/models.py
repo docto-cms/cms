7ad1a101a3b8f2a92b0e86486b7aba916ab74c70
docto-cms <doctocms@gmail.com>
2025-01-30 14:26:51 +0530

    patient

--- docto/Patient/models.py
+++ docto/Patient/models.py
@@ -11,7 +11,7 @@ class Patient(models.Model):
     Doctor = models.CharField(max_length=100)
     Fee = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     ConsultationFee = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    FeeType = models.CharField(max_length=100, null=True, blank=True)  # Allow null values
+    FeeType = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f'{self.RegistrationId} {self.FirstName} {self.LastName}'
