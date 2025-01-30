439598a0bdc3304263c2e2b633ab7322bdd2a6ff
Jeslin1999 <jeslinelizabeth8606@gmail.com>
2025-01-30 16:47:10 +0530

    appointment

--- docto/docto/settings.py
+++ docto/docto/settings.py
@@ -78,22 +78,13 @@ WSGI_APPLICATION = 'docto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'cmsg',
-        'USER':'root',
-        'PASSWORD':'root123',
-        'HOST': 'localhost',  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
