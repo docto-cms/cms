ee879d38d99a21da5fa3e34cd845a250b77d85f3
docto-cms <doctocms@gmail.com>
2025-01-31 11:45:17 +0530

    error

--- docto/regester/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns=[
-    path('regester',RegisterView.as_view()),
-    path("login",LoginView.as_view()),
-    path('logout',LogoutView.as_view()),
-    path("ChangePassword",ChangePassword.as_view())
-]
