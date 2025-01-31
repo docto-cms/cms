e670532da741a14763ec83eb52c90fc36164324c
docto-cms <doctocms@gmail.com>
2025-01-31 11:53:33 +0530

    error

---
+++ docto/Patient/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Patient(models.Model):
+    RegistrationId = models.CharField(max_length=100, unique=True)
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    PhoneNumber = models.CharField(max_length=15)  # Changed from IntegerField to CharField
+    Email = models.EmailField(max_length=100)
+    Age = models.IntegerField(null=True)
+    Gender = models.CharField(max_length=100)
+    Doctor = models.CharField(max_length=100)
+    Fee = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    ConsultationFee = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    FeeType = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.RegistrationId} {self.FirstName} {self.LastName}'
---
+++ docto/regester/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns=[
+    path('regester',RegisterView.as_view()),
+    path("login",LoginView.as_view()),
+    path('logout',LogoutView.as_view()),
+    path("ChangePassword",ChangePassword.as_view())
+]
