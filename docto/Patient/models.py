6ebcd48c696683bdba58e9ea5420ae3f2e92d56b
Nandakishor <nandhunandakishor@gmail.com>
2025-02-13 10:37:21 +0530

    change

--- docto/Patient/models.py
+++ docto/Patient/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-from appointment.models import Doctor
+
+
+class Doctor(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    specialization = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.firstname
 
 class Patient(models.Model):
 
--- docto/appointment/models.py
+++ docto/appointment/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from Patient.models import *
 
 
-class Doctor(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    specialization = models.CharField(max_length=255, blank=True, null=True)
-
-    def __str__(self):
-        return self.firstname
-
 #mobile
 class PatientAppointment(models.Model):
     first_name = models.CharField(max_length=255)
