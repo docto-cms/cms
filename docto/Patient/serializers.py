e92f801dd46ff057c73111cede4b9697571fc3bb
Jeslin1999 <jeslinelizabeth8606@gmail.com>
2025-02-13 15:35:01 +0530

    chnages

--- docto/Patient/serializers.py
+++ docto/Patient/serializers.py
@@ -2,9 +2,15 @@ from rest_framework import serializers
 from .models import *
 
 class PatientSerializer(serializers.ModelSerializer):
+    doctor_name = serializers.CharField(source='Doctor.firstname', default="No Doctor Assigned", read_only=True)
+
     class Meta:
-        model=Patient
-        fields='__all__'
+        model = Patient
+        fields = [
+            'RegistrationId', 'FirstName', 'LastName', 'PhoneNumber', 
+            'Email', 'Age', 'Gender', 'City', 'doctor_name', 'RefferedBy', 
+            'Fee', 'FeeType'
+        ]
 
 class BasicInfoSerializer(serializers.ModelSerializer):
     class Meta:
--- docto/Patient/views.py
+++ docto/Patient/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Patient
+from .models import *
 from .serializers import *
-from .models import Doctor
 
 class PatientDetailAPIView(APIView):
 
