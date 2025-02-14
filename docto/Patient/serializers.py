38a0f76d7dc5a08be9e697b6f3cc5496295b6292
Jeslin1999 <jeslinelizabeth8606@gmail.com>
2025-02-14 10:37:33 +0530

    msg

--- docto/Patient/serializers.py
+++ docto/Patient/serializers.py
@@ -2,15 +2,9 @@ from rest_framework import serializers
 from .models import *
 
 class PatientSerializer(serializers.ModelSerializer):
-    doctor_name = serializers.CharField(source='Doctor.firstname', default="No Doctor Assigned", read_only=True)
-
     class Meta:
         model = Patient
-        fields = [
-            'RegistrationId', 'FirstName', 'LastName', 'PhoneNumber', 
-            'Email', 'Age', 'Gender', 'City', 'doctor_name', 'RefferedBy', 
-            'Fee', 'FeeType'
-        ]
+        fields = ['__all__']
 
 class BasicInfoSerializer(serializers.ModelSerializer):
     class Meta:
